94381a7bbacef87599852e2527007c774e6f1d66
José Eduardo <eduardoaraujodev@gmail.com>
2022-11-07 12:21:59 -0300

    Pagination 90% done. Only a few minor details to adjust remaining.

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -1,9 +1,42 @@
 import { usePagination } from "store";
 
-const Pagination = () => {
-  const { currentPage } = usePagination();
+import styles from "./Pagination.module.css";
 
-  return <div style={{ textAlign: "right" }}>PAGINATION {currentPage}</div>;
+const Pagination = () => {
+  const { currentPage, setCurrentPage, getPagination } = usePagination();
+
+  return (
+    <div style={{ textAlign: "right" }}>
+      <button
+        disabled={currentPage === 1}
+        onClick={() => setCurrentPage((prevState) => prevState - 1)}
+      >
+        BACK
+      </button>
+      {getPagination(currentPage, 72).map((page) => {
+        if (page === currentPage) {
+          return (
+            <button
+              className={`${styles.linkButton} ${styles.currentButton}`}
+              onClick={() => setCurrentPage(page)}
+            >
+              {page}
+            </button>
+          );
+        }
+
+        return (
+          <button
+            className={styles.linkButton}
+            onClick={() => setCurrentPage(page)}
+          >
+            {page}
+          </button>
+        );
+      })}
+      <button>FORWARD</button>
+    </div>
+  );
 };
 
 export default Pagination;
--- src/store/pagination-context.tsx
+++ src/store/pagination-context.tsx
@@ -11,11 +11,48 @@ import {
 interface PaginationProviderInterface {
   currentPage: number;
   setCurrentPage: Dispatch<SetStateAction<number>>;
+  getPagination: (currentPage: number, totalResults: number) => number[];
 }
+
+const getPagination = (currentPage: number, totalResults: number) => {
+  const lastPage = Math.ceil(totalResults / 12);
+
+  if (currentPage > lastPage) {
+    return [];
+  }
+
+  const pages: number[] = [];
+
+  if (lastPage <= 5) {
+    for (let i = 1; i <= lastPage; i++) {
+      pages.push(i);
+    }
+
+    return pages;
+  }
+
+  if (currentPage > 3) {
+    let start = currentPage - 3 + 1;
+    const end = start + 5 > lastPage ? lastPage : start + 5;
+
+    if (end - start < 4) {
+      start = end - 4;
+    }
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    return pages;
+  }
+
+  return [1, 2, 3, 4, 5];
+};
 
 const initialState = {
   currentPage: 1,
   setCurrentPage: () => {},
+  getPagination,
 };
 
 const PaginationContext =
@@ -25,7 +62,9 @@ const PaginationProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
-    <PaginationContext.Provider value={{ currentPage, setCurrentPage }}>
+    <PaginationContext.Provider
+      value={{ currentPage, setCurrentPage, getPagination }}
+    >
       {children}
     </PaginationContext.Provider>
   );
